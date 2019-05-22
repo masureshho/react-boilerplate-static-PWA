328c124e1eea9346f1730dc719e96be67996dffe
Suresh Mainali <masureshho@gmail.com>
2019-05-22 18:00:56 +0545

    Clean code

---
+++ app/containers/AboutPage/index.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * AboutPage
+ *
+ */
+
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export function AboutPage() {
+  return (
+    <div>
+      <Helmet>
+        <title>AboutPage</title>
+        <meta name="description" content="Description of AboutPage" />
+      </Helmet>
+      About Page
+    </div>
+  );
+}
+
+export default AboutPage;
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,9 +8,10 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
+import AboutPage from 'containers/AboutPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
@@ -18,8 +19,22 @@ import GlobalStyle from '../../global-styles';
 export default function App() {
   return (
     <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
